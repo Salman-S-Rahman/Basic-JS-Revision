d0566f0455e58e193b4f9eb8f6255838a0a53fa1
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-13 23:53:05 +0600

    added

---
+++ const.js
@@ -0,0 +1,13 @@
+let firstName1 = "Taskin"
+let lastName1 = " Asghar"
+lastName1 = " Afghan"
+let fullName1 = firstName1 + lastName1;
+
+console.log(fullName1);
+
+const firstName = "Taskin"
+const lastName = " Asghar"
+lastName = " Afghan"
+// ========== Assignment to constant variable. ============
+const fullName = firstName + lastName;
+console.log(fullName);
