48478cc16b3c29d81825b5c407f11317661a7996
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-13 23:49:20 +0600

    added

---
+++ let.js
@@ -0,0 +1,13 @@
+let firstName = "Taskin"
+let lastName = " Asghar"
+let fullName = firstName + lastName;
+console.log(fullName);
+
+
+
+let firstName1 = "Taskin"
+let lastName1 = " Asghar"
+lastName1 = " Afghan"
+let fullName1 = firstName1 + lastName1;
+
+console.log(fullName1);
