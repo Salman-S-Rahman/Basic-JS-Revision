015b36e86c0ad24db8a71bf020976d729217ad71
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-27 23:36:37 +0600

    added

--- math-operation.js
+++ math-operation.js
@@ -1,4 +1,12 @@
 var price1 = 100;
 var price2 = 5;
+
 var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+
 console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
