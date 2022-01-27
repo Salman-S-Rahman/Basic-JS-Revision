1edf8f274a5928c0005c0e7400f54b3ed6079ca4
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-27 23:48:14 +0600

    added

--- stringConversion.js
+++ stringConversion.js
@@ -2,3 +2,12 @@ var firstName = "Taskin";
 var lastName = "Asghar";
 var fullName = firstName + " " + lastName;
 console.log("fullName = " + fullName);
+
+// === string concatenation ==
+
+var price1 = '43';
+var price2 = '42';
+var total1 = price1 + price2;
+console.log(total1);
+
+// ===== sum not possible between 1 string & 1 number 
