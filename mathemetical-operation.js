666a3ab2caae72ec9d16e200ce65216aa0ee198d
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-27 23:13:14 +0600

    mathmetical operation

--- mathemetical-operation.js
+++ mathemetical-operation.js
@@ -1,4 +1,16 @@
-var onionPrice = 42;
+var onionPrice = 40;
 var eggPrice = 43;
+var eggQuantity = 5;
+var totalAmount = 120;
+
 var totalPrice = onionPrice + eggPrice;
-console.log("Total Price = " + totalPrice);
+console.log("Total Price = " + totalPrice + "Tk");
+
+var priceDifference = eggPrice - onionPrice;
+console.log("Price Difference = " + priceDifference + "Tk");
+
+var totalEggPrice = eggPrice * eggQuantity;
+console.log("Total Egg Price = " + totalEggPrice + "Tk");
+
+var onionAmount = totalAmount / onionPrice;
+console.log("Total Oninon = " + onionAmount + "kg");
