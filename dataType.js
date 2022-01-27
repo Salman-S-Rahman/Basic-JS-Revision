77dc51a67c4cbae845d3881071ed574d36cd9916
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-28 00:18:38 +0600

    data type check

--- dataType.js
+++ dataType.js
@@ -3,7 +3,7 @@ var value2 = 43;
 var value3 = true;
 var value4;
 
-console.log("Data Type ="  + " " + value1);
-console.log("Data Type ="  + " " + value2);
-console.log("Data Type ="  + " " + value3);
-console.log("Data Type ="  + " " + value4);
+console.log("Data Type = " + typeof(value1));
+console.log("Data Type = " + typeof(value2));
+console.log("Data Type = " + typeof(value3));
+console.log("Data Type = " + typeof(value4));
