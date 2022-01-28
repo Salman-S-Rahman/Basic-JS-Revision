da6d5ef94be24e146a737a7f2d6cc0a41041a361
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-28 19:16:38 +0600

    array index & length

---
+++ array.js
@@ -0,0 +1,14 @@
+var numbers = [11, 32, 98, 54, 38, 22, 89];
+
+console.log(numbers.indexOf(5));
+
+console.log(numbers.indexOf(32));
+
+console.log(numbers.indexOf(89));
+
+// ====== to know index of different element in array =======
+
+var lengthOfNum = numbers.length;
+console.log("Total Element In Array Is : " + lengthOfNum);
+
+// ======== to know length of an array =====
