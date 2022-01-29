64a94725abad0e6c1ccb6a94551aefcfeae8f2c4
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-29 15:49:35 +0600

    forLoop

--- forLoop.js
+++ forLoop.js
@@ -5,3 +5,17 @@ for (var x = 1; x <= 10; x++) {
 for (var x = 1; x <= 10; x++) {
     console.log(x);
 }
+
+// ===== from 1 to 10 ====
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+// ===== from 10 to 1 ======
+
+for (var i = 2; i <= 40; i = i + 2) {
+    console.log(i);
+}
+
+// ======= from 2 to 100 
