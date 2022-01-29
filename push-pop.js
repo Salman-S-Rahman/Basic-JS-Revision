149948aba7c8911d9cba6100b77f82b063f1bc24
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-29 12:58:58 +0600

    push-pop

--- push-pop.js
+++ push-pop.js
@@ -1,4 +1,20 @@
 var names = ['kalam', 'salam', 'balam'];
 console.log("Before Push = " + names);
 names.push("jalam");
+console.log("After Push = " + names);
+
+// ====== push add element from the last =======
+
+names.pop();
 console.log("After Pop = " + names);
+
+names.pop();
+console.log("After Pop Again = " + names);
+
+names.pop();
+console.log("After Pop Again & Again = " + names);
+
+names.pop();
+console.log("After Pop Again, Again & Again = " + names);
+
+// ====== push remove element from the last =======
