7481782d95bc8217991d8a9ea08287c25cf4fc5c
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-29 13:08:24 +0600

    shift-unshift

--- shift-unshift.js
+++ shift-unshift.js
@@ -5,3 +5,19 @@ console.log("Before Unshift = " + names);
 names.unshift("Jalam");
 
 console.log("After Unshift = " + names);
+
+// ======= unshift add the value from the starting of the array ======
+
+names.shift();
+console.log("After shift = " + names);
+
+names.shift();
+console.log("After shift = " + names);
+
+names.shift();
+console.log("After shift = " + names);
+
+names.shift();
+console.log("After shift = " + names);
+
+// ======= shift remove the value from the starting of the array ======
