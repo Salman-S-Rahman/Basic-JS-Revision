f31377f41bffbcc450d6f0566c82fe8748c481d3
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-29 13:57:23 +0600

    conditional js

---
+++ condition.js
@@ -0,0 +1,15 @@
+var number = 50;
+
+if (number >= 90) {
+    console.log("You Got A+");
+} else if (number >= 80 && number < 90) {
+    console.log("You Got A");
+} else if (number >= 70 && number < 80) {
+    console.log("You Got A-");
+} else if (number >= 60 && number < 70) {
+    console.log("You Got B+");
+} else if (number >= 50 && number < 60) {
+    console.log("You Got B");
+} else {
+    console.log("You Have Failed");
+}
