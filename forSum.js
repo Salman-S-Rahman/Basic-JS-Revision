361eb971eb0816cb8d7b32bdd493378d7c3f3428
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-30 22:49:29 +0600

    added

---
+++ forSum.js
@@ -0,0 +1,11 @@
+let total = 0;
+for (var i = 0; i <= 5; i++) {
+    total = total + i;
+}
+console.log(total);
+
+let total1 = 0;
+for (let i = 0; i <= 50; i++) {
+    total1 = total1 + i;
+}
+console.log(total1);
