003f6b16f175f9ac0a09676d8c2d31a639f3adee
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-31 20:19:34 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 7;
+
+console.log("Before Swap x = " + x + " & y = " + y);
+
+var temp = y;
+y = x;
+x = temp;
+
+console.log("After Swap x = " + x + " & y = " + y);
+
+
+// ============ one way ===============
