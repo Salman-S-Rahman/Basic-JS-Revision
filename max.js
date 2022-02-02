722ec3c92ddad85576e649c9d30bd07183f3ee7f
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-02 11:35:21 +0600

    random value

--- max.js
+++ max.js
@@ -13,3 +13,17 @@ var minValue = Math.min(50, 41, 65, 23, 76, 12, 09, 76, 4);
 console.log("Min value is = " + minValue);
 
 // ========== finding min value by math object ===========
+
+// function valueGenerator(n) {
+//     for (var i = 0; i < 10; i++) {
+//         var result = Math.floor(Math.random() * n + 1);
+//     }
+//     return result;
+
+
+// }
+// var total = valueGenerator(10);
+// console.log(total);
+
+
+// ========== it doesnt't work
