aabae00c5399739ff217612ab0dbb3c82a2ed095
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-04 19:57:12 +0600

    divisible-by

--- divisible-by.js
+++ divisible-by.js
@@ -2,5 +2,11 @@ for (var i = 1; i <= 50; i++) {
   const element = i;
   if (element % 3 == 0 && element % 5 == 0) {
     console.log("foobar");
+  } else if (element % 3 == 0) {
+    console.log("foo");
+  } else if (element % 5 == 0) {
+    console.log("bar");
+  } else {
+    console.log(i);
   }
 }
