fcbc8d545ad102f15d9b12968aa984730bdd8490
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-04 17:32:33 +0600

    object property

--- object-property.js
+++ object-property.js
@@ -4,3 +4,11 @@ const product = {
   brand: "Samsung",
   price: 20000,
 };
+console.log(product);
+const productPrice = "price";
+
+// ====== object value can be passed in three way =====
+console.log(product.brand);
+console.log(product["title"]);
+product[productPrice] = 40000;
+console.log(product[productPrice]);
