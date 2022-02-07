f18827834c0db204389605d396223391e9a1080d
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-07 13:06:43 +0600

    factorily

--- fact.js
+++ fact.js
@@ -15,3 +15,14 @@ while (i <= 10) {
 console.log(factorial);
 
 // ===== factorial in iterative way by while loop =========
+function fact(n) {
+  var factorial = 1;
+  for (var i = 1; i <= n; i++) {
+    var factorial = factorial * i;
+  }
+  return factorial;
+}
+var result = fact(5);
+console.log(result);
+
+// ===== factorial in iterative way by loop & function =========
