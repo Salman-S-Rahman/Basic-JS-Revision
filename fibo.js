c462a10013ee917d2dfbb583dea0fad5b90420ec
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-08 22:50:24 +0600

    factorial recursive

--- fibo.js
+++ fibo.js
@@ -1,8 +1,15 @@
-var fibo = [0, 1]
+var fibo = [0, 1];
 
 for (var i = 2; i < 10; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
-console.log(fibo)
+console.log(fibo);
 
-// ============= getting fibonacci array in iterative way ========== 
+// ============= getting fibonacci array in iterative way ==========
+var fibo = [0, 1];
+for (var i = 2; i <= 10; i++) {
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
+  console.log(fibo[i], fibo[i - 1], fibo[i - 2]);
+}
+
+// ============= getting fibonacci  10 numbers in iterative way ==========
