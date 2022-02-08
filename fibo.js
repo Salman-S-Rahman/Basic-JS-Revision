07c093a803b9385a7c3d2f9baddddd4f072f8284
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-08 22:49:39 +0600

    factorial recursive

---
+++ fibo.js
@@ -0,0 +1,8 @@
+var fibo = [0, 1]
+
+for (var i = 2; i < 10; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+console.log(fibo)
+
+// ============= getting fibonacci array in iterative way ========== 
