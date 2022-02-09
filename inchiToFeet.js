12c9a5129667d50c26da2322797f4fdd287558d5
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-09 23:21:39 +0600

    inchi to feet

--- inchiToFeet.js
+++ inchiToFeet.js
@@ -7,3 +7,12 @@ var result = inchiToFeet(3);
 console.log(result);
 
 // ============ with return value =============
+
+function feet(n) {
+  inchi = 12 * n;
+  console.log(n + " inchi = " + inchi + " Feet");
+}
+
+feet(4);
+
+// ============ with return value =============
