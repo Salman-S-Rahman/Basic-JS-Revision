c14e49975dc5a27d5a3fb2e6dd6a8951bfcc64f3
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-09 23:19:28 +0600

    leap year

--- leapYear.js
+++ leapYear.js
@@ -1,12 +1,23 @@
 function leapYear(n) {
-    if (n % 4 == 0 && n % 100 !== 0 || n % 400 == 0) {
-        console.log(n + " Is Leap Year");
-    } else {
-        console.log(n + " Is Leap Year")
-    }
+  if ((n % 4 == 0 && n % 100 !== 0) || n % 400 == 0) {
+    console.log(n + " Is Leap Year");
+  } else {
+    console.log(n + " Is Leap Year");
+  }
 }
 
 leapYear(2010);
 
-
 // ========= without return the value =========
+
+function leapYearCheck(n) {
+  if ((n % 4 == 0 && n % 100 !== 0) || n % 400 == 0) {
+    return n + " Is Leap Year";
+  } else {
+    return n + " Is Leap Year";
+  }
+}
+var result = leapYearCheck(2011);
+console.log(result);
+
+// ========== with return value from function =============
