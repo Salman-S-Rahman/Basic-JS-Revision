66b192e951622c26c66de927fcc46dafd6978fff
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-24 13:54:10 +0600

    all country shwn in ui

--- JS Api Practice/Countries/app.js
+++ JS Api Practice/Countries/app.js
@@ -1,7 +1,24 @@
 const loadCountries = () => {
   fetch("https://restcountries.com/v2/all")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayCountries(data));
 };
 
-loadCountries()
+loadCountries();
+
+const displayCountries = (countries) => {
+  const countriesContainer = document.getElementById("countries");
+  countries.forEach((country) => {
+    console.log(country);
+    const div = document.createElement("div");
+    div.classList.add("country");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    img.src = country.flag;
+    h2.innerText = country.name;
+    div.appendChild(img);
+    div.appendChild(h2);
+
+    countriesContainer.appendChild(div);
+  });
+};
