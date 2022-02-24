7f6b22a46fd72b2dcc9ce6b4e4d10d4c029acd82
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-24 20:15:01 +0600

    countries info

--- JS Api Practice/Countries Info/js/script.js
+++ JS Api Practice/Countries Info/js/script.js
@@ -1,10 +1,30 @@
 const loadCountries = () => {
   fetch("https://restcountries.com/v2/all")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayCountries(data));
 };
-loadCountries();
 
-const displayCountries = countries =>{
-    
-}
+const displayCountries = (countries) => {
+  const countriesDiv = document.getElementById("countries");
+  countries.forEach((country) => {
+    console.log(country);
+    const div = document.createElement("div");
+    div.classList.add("country-all");
+    div.classList.add("col");
+    div.innerHTML = `
+         <div>
+            <div class="d-flex justify-content-start">
+                  <img src=${country.flag} alt="">
+                  <h6>${country.name}</h6>
+            </div>
+            <div>
+                  <h6>Capital: ${country.capital}</h6>
+                  <h6>Population: ${country.population}</h6>
+                  <h6>Region: ${country.region}</h6>
+                  <h6>Time Zone: ${country.timezones[0]} </h6>
+            </div>
+      </div>
+    `;
+    countriesDiv.appendChild(div);
+  });
+};
