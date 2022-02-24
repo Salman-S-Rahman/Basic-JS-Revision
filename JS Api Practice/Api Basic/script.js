58b49ea2f22be62928892caf2b317435ee03dbd3
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-24 12:05:54 +0600

    dynamically user list

--- JS Api Practice/Api Basic/script.js
+++ JS Api Practice/Api Basic/script.js
@@ -1,5 +1,15 @@
-const loadUser = () =>{
+const loadUser = () => {
   fetch("https://jsonplaceholder.typicode.com/users")
-  .then(res => res.json())
-  .then(data => console.log(data))
+    .then((res) => res.json())
+    .then((data) => displayUser(data));
+};
+
+function displayUser(data) {
+  console.log(data);
+  const ul = document.getElementById("user-list");
+  for (const user of data) {
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.appendChild(li);
+  }
 }
