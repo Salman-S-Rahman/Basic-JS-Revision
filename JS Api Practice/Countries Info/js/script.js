e025de1ba4e5414a59185b22ea781c091fb7ba71
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-25 18:55:02 +0600

    Eu countries load

--- JS Api Practice/Countries Info/js/script.js
+++ JS Api Practice/Countries Info/js/script.js
@@ -35,7 +35,31 @@ const displayCountries = (countries) => {
 const loadEuCountries = () => {
   fetch("https://restcountries.com/v2/regionalbloc/eu")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayEuCountries(data));
 };
 
-loadEuCountries();
+const displayEuCountries = (countries) => {
+  const euCountriesDiv = document.getElementById("euCountries");
+  countries.forEach((country) => {
+    console.log(country);
+    const div = document.createElement("div");
+    div.classList.add("country-all");
+    div.classList.add("col");
+    div.innerHTML = `
+         <div>
+            <div class="d-flex justify-content-start">
+                  <img src=${country.flag} alt="">
+                  <h6>${country.name}</h6>
+            </div>
+            <div>
+                  <h6>Capital: ${country.capital}</h6>
+                  <h6>Population: ${country.population}</h6>
+                  <h6>Region: ${country.region}</h6>
+                  <h6>Time Zone: ${country.timezones[0]} </h6>
+            </div>
+      </div>
+    `;
+
+    euCountriesDiv.appendChild(div);
+  });
+};
