9d75c3d1e8cb16114579be4e4b35c9a356f3a5f4
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-26 23:44:06 +0600

    meal details search by mealid

--- JS Api Practice/Mealdb/js/app.js
+++ JS Api Practice/Mealdb/js/app.js
@@ -33,5 +33,41 @@ const displayMeal = (mealId) => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayMealDetails(data.meals));
 };
+
+const displayMealDetails = (meals) => {
+  console.log(meals);
+  const mealDetails = document.getElementById("meal-details");
+
+  meals.forEach((meal) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+              <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="..."    style="height:400px">
+               <div class="card-body text-left">
+                          <h3 class="card-title">${meal.strMeal}</h3>
+                          <h4>Ingredients</h4>
+                          <div id="ingredients">
+                          </div>
+               </div>
+
+               <div class="card-footer text-center">
+                  <small class="text-muted">${meal.strCategory}</small>
+               </div>
+              `;
+    mealDetails.appendChild(div);
+    const mealIngredients = document.getElementById("ingredients");
+
+    for (let i = 1; i <= 20; i++) {
+      if (meal["strIngredient" + i]) {
+        const ingredient = document.createElement("p");
+        ingredient.innerText = `${meal["strMeasure" + i]} ${
+          meal["strIngredient" + i]
+        }`;
+        mealIngredients.appendChild(ingredient);
+      }
+    }
+  });
+};
+
+// ====== meal details =======
