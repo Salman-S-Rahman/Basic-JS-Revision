bd158ec54bed6e5b3bac4e3f086a99ac97431bed
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-27 00:06:20 +0600

    meal details search by mealid

--- JS Api Practice/Mealdb/js/app.js
+++ JS Api Practice/Mealdb/js/app.js
@@ -58,6 +58,16 @@ const displayMealDetails = (meals) => {
     mealDetails.appendChild(div);
     const mealIngredients = document.getElementById("ingredients");
 
+    for (var i = 1; i <= 20; i++) {
+      if (meal["strIngredient" + i]) {
+        const ingredient = document.createElement("p");
+        ingredient.innerText = `${meal["strMeasure" + i]} ${
+          meal["strIngredient" + i]
+        } `;
+        mealIngredients.appendChild(ingredient);
+      }
+    }
+
     for (let i = 1; i <= 20; i++) {
       if (meal["strIngredient" + i]) {
         const ingredient = document.createElement("p");
