7e16c31f4fbe530032bb186e7dc3079e2fbc95d2
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-27 21:03:46 +0600

    drink details shown

--- JS Api Practice/Cocktaildb/js/script.js
+++ JS Api Practice/Cocktaildb/js/script.js
@@ -1,7 +1,11 @@
 const searchField = document.getElementById("search-field");
 const searchValue = searchField.value;
+const spinner = document.getElementById("spinner");
+const error1 = document.getElementById("error1");
+const error2 = document.getElementById("error2");
 
 const loadDrinks = () => {
+  spinner.style.display = "block";
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
   fetch(url)
     .then((res) => res.json())
@@ -21,6 +25,7 @@ const displayDrinks = (drinks) => {
       </div> 
     `;
     singleDrink.appendChild(div);
+    spinner.style.display = "none";
   });
 };
 
@@ -28,6 +33,7 @@ const displayDrinks = (drinks) => {
 
 const displayDrink = (drinkId) => {
   console.log(drinkId);
+  spinner.style.display = "block";
   const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
   fetch(url)
     .then((res) => res.json())
@@ -40,7 +46,28 @@ const displayDetails = (drinks) => {
   drinks.forEach((drink) => {
     const div = document.createElement("div");
     div.innerHTML = `
-            
+            <img src="${drink.strDrinkThumb}" class="card-img-top img-fluid" alt="..." style="height:400px">
+                    <div class="card-body text-left">
+                        <h3 class="card-title">${drink.strGlass}</h3>
+                        <h4>Ingredients::</h4>
+                        <div id="ingredients">
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <small class="text-muted fw-bold">${drink.strCategory}</small>
+                    </div>
     `;
+    drinkDetails.appendChild(div);
+    const drinkIngredients = document.getElementById("ingredients");
+    for (var i = 1; i <= 15; i++) {
+      if (drink["strIngredient" + i]) {
+        const p = document.createElement("p");
+        p.innerHTML = `${drink["strIngredient" + i]} ${
+          drink["strMeasure" + i]
+        }`;
+        drinkIngredients.appendChild(p);
+      }
+    }
   });
+  spinner.style.display = "none";
 };
