906893790b12486c643bfb253279b3e5af380329
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-13 11:14:05 +0600

    tempplate string

--- ES6 Practice/templateString.js
+++ ES6 Practice/templateString.js
@@ -1,1 +1,12 @@
-console.log("Template String");
+var numbers = [22, 25, 27, 65, 87, 37];
+var studentsInfo = {
+  name: "Taskin Asghar",
+  study: {
+    uni: "AIUB",
+    subject: "EEE",
+  },
+};
+
+const myInfo = `My name is ${studentsInfo.name} & I'm ${numbers[1]} years old. I study at ${studentsInfo.study.uni} in ${studentsInfo.study.subject}`;
+
+console.log(myInfo);
